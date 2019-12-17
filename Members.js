ebc358f97717c46d1c9436e669b8f348a86ae3af
徐 <45097681+xv700@users.noreply.github.com>
2019-12-17 21:49:43 +0800

    Update Members.js

--- Members.js
+++ Members.js
@@ -1,11 +1,11 @@
-const Members=[{
+const Members=[
+{
 Applicant:"https://github.com/xv700",//your git url 
 Approval:"Yes",
 Application_status:"complete"
 },
 {
 Applicant:"",
-Approval:"",
-Application_status:""
+
 }
 ]
