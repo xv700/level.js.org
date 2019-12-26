7d1546b343f0e11af7170ed176af8caf61f5b188
jiamu1314 <58981359+jiamu1314@users.noreply.github.com>
2019-12-26 21:25:00 +0800

    申请加入level.js.org

--- Members.js
+++ Members.js
@@ -10,8 +10,8 @@ Applicant:"你的git地址",
 Approval:"",  //是否批准加入level组织，申请时候请留空
 },
 {
-index:1,//索引 ，demo
+index:1,
 Applicant:"https://github.com/jiamu1314",
-Approval:"",  //是否批准加入level组织，申请时候请留空
+Approval:"",  
 }
 ]
