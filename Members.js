d62b5288d3cd4f8c34d8f33e1a7491f7b8423f1f
徐 <45097681+xv700@users.noreply.github.com>
2019-12-27 10:46:38 +0800

    Update Members.js

--- Members.js
+++ Members.js
@@ -5,7 +5,7 @@ Applicant:"https://github.com/xv700",//your git url
 Approval:"Yes",  //是否批准加入level组织
 },
 {
-index:0,//索引 ，demo
+index:0,//索引 填写时候留空 ，demo 
 Applicant:"你的git地址",
 Approval:"",  //是否批准加入level组织，申请时候请留空
 },
